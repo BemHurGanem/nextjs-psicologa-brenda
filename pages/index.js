0e3da4410239e1a7048ef7947003230e40d3f717
Bem Hur <bemhur.ganem@gmail.com>
2021-02-22 19:11:13 -0300

    presentation

--- pages/index.js
+++ pages/index.js
@@ -20,12 +20,6 @@ export default function Home() {
           <a href="">
             <Image src="/images/logo.png" className="header-logo" width={70} height={70} priority={true} />
           </a>
-
-          {/* <label htmlFor="menu-toggle" className="pointer-cursor lg:hidden block"><svg className="fill-current text-gray-900"
-          xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
-          <title>menu</title>
-          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
-        </svg></label> */}
           <input className="hidden" type="checkbox" id="menu-toggle" />
 
           <div className="flex items-center justify-end lg:w-auto w-full" id="menu">
@@ -36,9 +30,6 @@ export default function Home() {
                   href="#">Sobre mim</a></li>
                 <li><a className="nav-link lg:p-4 py-3 px-0 block border-b-2 border-transparent mx-3 "
                   href="#">Serviços</a></li>
-                {/* <li> */}
-                {/* <a className="nav-link flex lg:p-4 py-3 px-0 block border-b-2 border-transparent  lg:mb-0 mb-2" */}
-                {/* href="#"><FontAwesomeIcon icon={faPhoneAlt} size="sm" /> (77) 991653808</a></li> */}
                 <li>
                   <a className="cta flex font-normal  py-2 px-5  block border-b-2 border-transparent bg-complementary rounded-full "
                     href="#"><FontAwesomeIcon icon={faWhatsapp} size="sm" /> Whatsapp</a>
@@ -47,40 +38,37 @@ export default function Home() {
             </nav>
           </div>
         </div>
-
-
       </header>
 
 
-      <section id="hero" className="w-full bg-white flex antialiased">
-        <div className="w-full md:w-3/5 relative h-full flex hero-info">
+      <section id="presentation" className="w-full bg-white flex antialiased">
+        <div className="w-full md:w-3/5 relative h-full flex presentation-text">
           <div className=" h-auto my-auto md:mx-auto z-10">
-            <div className=" font-extrabold hero-title-margin">
-              <h1 className=" hero-subtitle  color-primary inline">Olá! Sou </h1>
-              <h1 className=" hero-subtitle  color-complementary inline">Brenda Fernanda </h1>
-              <h1 className=" hero-subtitle color-primary md:inline ">e trabalho com</h1>
-              <h1 className=" color-primary mt-4 xm:mt-9 hero-title">Psicoterapia para</h1>
-              <h1 className=" color-complementary mt-1 md:inline hero-title">Crianças e </h1>
-              <h1 className=" color-complementary mt-1 md:inline hero-title">Adolescentes</h1>
+            <div className=" font-extrabold presentation-text-margin">
+              <h1 className=" presentation-subtitle  color-primary inline">Olá! Sou </h1>
+              <h1 className=" presentation-subtitle  color-complementary inline">Brenda Fernanda </h1>
+              <h1 className=" presentation-subtitle color-primary md:inline ">e trabalho com</h1>
+              <h1 className=" color-primary mt-4 xm:mt-9 presentation-title">Psicoterapia para</h1>
+              <h1 className=" color-complementary mt-1 md:inline presentation-title">Crianças e </h1>
+              <h1 className=" color-complementary mt-1 md:inline presentation-title">Adolescentes</h1>
             </div>
             <div className="text-base xl:text-xl mt-9 xm:mt-12">
-              <button className="cta bg-complementary rounded-full  px-6 py-3 hero-title-margin">
+              <button className="cta bg-complementary rounded-full  px-6 py-3 presentation-text-margin">
                 Entre em contato
               </button>
             </div>
           </div>
-          <div className="hero-img-container-mobile">
+          <div className="presentation-img-container-mobile">
             <div className=" flex md:hidden z">
-              <Image src="/images/brenda-mobile.png" className="hero-img-mobile" alt="Brenda Fernanda" width={317} height={618} priority={true} quality={100} />
+              <Image src="/images/brenda-mobile.png" className="presentation-img-mobile" alt="Brenda Fernanda" width={317} height={618} priority={true} quality={100} />
             </div>
           </div>
-
 
         </div>
         <div className="w-0 md:w-2/5 h-full flex pink-background">
           <div className="w-full items-end hidden md:flex">
-            <div className="mx-auto hero-img-container flex">
-              <Image src="/images/brenda.png" className="hero-img" alt="Brenda Fernanda" width={317} height={618} priority={true} quality={100} />
+            <div className="mx-auto presentation-img-container flex">
+              <Image src="/images/brenda.png" className="presentation-img" alt="Brenda Fernanda" width={317} height={618} priority={true} quality={100} />
             </div>
           </div>
         </div>
